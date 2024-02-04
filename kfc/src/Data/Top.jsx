d80badc08311e21ed5364c60357262c5c704fc20
Syedshahwarhassan <Syedshahwarhassan87@gmail.com>
2024-02-04 13:07:16 +0500

    complete

--- kfc/src/Data/Top.jsx
+++ kfc/src/Data/Top.jsx
@@ -6,5 +6,29 @@ export const top=[
     des:"Twister + 1 Regular fries + 1 Regular drink",
     image:"https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-twister-combo-2023-05-31115706.png",
     price:670
-    }
+    },
+    {
+        
+        id:2,
+        title:"Zinger Stacker Combo",
+        des:"1 Zinger Stacker + 1 Regular fries + 1 Regular drink",
+        image:"https://www.kfcpakistan.com/images/43a98620-ffaa-11ed-b6b3-6970cc1cd666-zingerstack-Combo-2023-05-31115706.png",
+        price:850
+        },
+        {
+        
+            id:3,
+            title:"Crispy Duo Box",
+            des:"Turn up the fun with 5 pcs Hot & Crispy Chicken + 1 Large fries + 2 Regular drinks",
+            image:"https://www.kfcpakistan.com/images/43a95f10-ffaa-11ed-b673-4121381f04c6-CrispyDuoBox-2023-05-31115706.png",
+            price:670
+            },
+            {
+        
+                id:4,
+                title:"Family Festival 3",
+                des:"An ultimate meal for the fam. It includes 4 Zinger burgers + 4 pieces Hot and Crispy Chicken + 2 Dinner rolls + 1.5 Liter drink",
+                image:"https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-family-Festivle-3-2023-05-31115706.png",
+                price:2450
+                },
 ]
